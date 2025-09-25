5f9981fdee781b5c0884c40e5411ba28612058a4
rickhancock-droid <rick.hancock@rhconsulting.ai>
2025-09-24 18:42:36 -0700

    Refactor fetch to use dynamic import

--- add-status-field.js
+++ add-status-field.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const GH_TOKEN = process.env.GH_TOKEN;
 const PROJECT_ID = process.env.PROJECT_ID;
